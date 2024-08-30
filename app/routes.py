a4d860a575a61e832c52bae41be73d6790b7eec6
Ekrem Emre <ekrem.emre@posteo.de>
2024-08-30 13:08:38 +0200

    added raw ics to route

--- app/routes.py
+++ app/routes.py
@@ -1,8 +1,9 @@
+import requests
 from datetime import datetime
 
-from flask import Blueprint, jsonify, request
+from flask import Blueprint, jsonify, request, Response
 
-from app.event_fetcher import fetch_events
+from app.event_fetcher import fetch_events, ICS_URL
 
 app = Blueprint('app', __name__)
 
@@ -23,3 +24,19 @@ def post_events():
 
     events = fetch_events(current_time)
     return jsonify(events)
+
+@app.route('/api/ics')
+def get_ics():
+    try:
+        response = requests.get(ICS_URL)
+
+        if response.status_code == 200:
+            return Response(
+                response=response.content,
+                mimetype="text/calendar",
+                headers={"Content-Disposition": f"attachment; filename=cto-calendar.ics"}
+            )
+        else:
+            return f"Failed to fetch ICS file. Status code {response.status_code}", 500
+    except requests.exceptions.RequestException as e:
+        return f"An error occured: {str(e)}", 500
