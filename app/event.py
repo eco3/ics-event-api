2b9d88eee1b0b91db9903b52f0fbfdc958b2d10d
Ekrem Emre <ekrem.emre@posteo.de>
2024-09-04 11:29:41 +0200

    added html sanitizer

--- app/event.py
+++ app/event.py
@@ -1,3 +1,4 @@
+import re
 from dataclasses import dataclass
 from typing import Optional
 from marshmallow_dataclass import class_schema
@@ -17,5 +18,23 @@ class Event:
     end: str
     is_all_day: bool
     recurrence: Optional[EventRecurrence]
+
+    def __post_init__(self):
+        self.description = self._clean_html(self.description) if self.description else ''
+
+    def _clean_html(self, input_string):
+        # Replace various tags that imply new lines with \n
+        input_string = re.sub(r'<br\s*/?>', '\n', input_string, flags=re.IGNORECASE)    # <br>
+        input_string = re.sub(r'<hr\s*/?>', '\n', input_string, flags=re.IGNORECASE)    # <hr>
+        
+        # Replace newline HTML entities with \n
+        input_string = re.sub(r'&#10;', '\n', input_string)  # ASCII Line Feed (LF)
+        input_string = re.sub(r'&#13;', '\n', input_string)  # ASCII Carriage Return (CR)
+        
+        # Remove all other HTML tags
+        cleaned_string = re.sub(r'<[^>]+>', ' ', input_string)
+        
+        # Strip leading/trailing whitespace
+        return cleaned_string.strip()
 
 EventSchema = class_schema(Event)
